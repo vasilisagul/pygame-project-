81d74cd2d53664098285ed8e597a0377fae635f4
vasilisagul <gulinavasilisa914@gmail.com>
2025-02-25 16:54:26 +0300

    Add files via upload

---
+++ Рекламная кампания.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def countdown():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    return '''Человечество вырастает из детства.<br>
+                Человечеству мала одна планета.<br>
+                Мы сделаем обитаемыми безжизненные пока планеты.<br>
+                И начнем с Марса!<br>
+                Присоединяйся!'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
