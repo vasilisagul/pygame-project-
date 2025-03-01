6d6058f2ec69ae13443e1559400b9cf83eafb815
admin <gulinavasilisa914@gmail.com>
2025-03-01 15:38:21 +0300

    123

---
+++ pygame project/main.py
@@ -0,0 +1,136 @@
+import os
+import sys
+
+import pygame
+
+FPS = 10
+WIDTH = 640
+HEIGHT = 480
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+pygame.init()
+
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.rect = None
+
+    def get_event(self, event):
+        pass
+
+
+class Tile(Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group)
+        # self.image = tile_images[tile_type]
+        # self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Board(Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group)
+        self.image = board_image
+        self.rect = self.image.get_rect().move(board_width * pos_x + 15, board_height * pos_y + 5)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(board_width * self.pos[0] + 15, board_height * self.pos[1] + 5)
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+# tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
+
+board_image = load_image('board1.png')
+
+board_width = board_image.get_width()
+board_height = board_image.get_height()
+
+
+# tile_width = tile_height = 50
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+    self.terminate()
+
+
+def main():
+    # pygame.init()
+    w, h = 600, 300
+    screen = pygame.display.set_mode((w, h))
+    pygame.display.set_caption('Игра «Жизнь»')
+    clock = pygame.time.Clock()
+    # all_sprites = pygame.sprite.Group(Car('img.png', w))
+    screen.fill((255, 255, 255))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # all_sprites.update()
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    start_screen()
+    main()
