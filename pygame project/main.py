3ae1830ae4bc03b6770985aa8b161fdd456e2d32
admin <gulinavasilisa914@gmail.com>
2025-03-01 21:33:07 +0300

    walls and ball movement

--- pygame project/main.py
+++ pygame project/main.py
@@ -20,6 +20,7 @@ all_sprites = pygame.sprite.Group()
 board_group = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 ball_group = pygame.sprite.Group()
+border_group = pygame.sprite.Group()
 
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
@@ -35,10 +36,17 @@ class Sprite(pygame.sprite.Sprite):
         pass
 
 
+# class Border(Sprite):
+#     def __init__(self, pos_x, pos_y):
+#         super().__init__(border_group)
+#         self.image = border_image
+#         self.rect = self.image.get_rect().move(pos_x, pos_y)
+#         # self.pos = [pos_x, pos_y]
+
 class Border(Sprite):
     # строго вертикальный или строго горизонтальный отрезок
     def __init__(self, x1, y1, x2, y2):
-        super().__init__(all_sprites)
+        super().__init__(border_group)
         if x1 == x2:  # вертикальная стенка
             self.add(vertical_borders)
             self.image = pygame.Surface([1, y2 - y1])
@@ -77,13 +85,11 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(ball_group)
         self.image = ball_image
-        self.rect = self.image.get_rect().move(pos_x - self.image.get_width() // 2, pos_y)
+        self.rect = self.image.get_rect().move(pos_x - self.image.get_width() // 2, pos_y - SIZE_BALL[0])
         self.pos = [pos_x, pos_y]
         print(self.pos)
         self.vx = random.randint(-5, 5)
         self.vy = random.randrange(-5, -1)
-        print(pos_x, pos_y)
-        print(self.vx, self.vy)
 
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
@@ -91,7 +97,7 @@ class Ball(pygame.sprite.Sprite):
             self.vy = -self.vy
         if pygame.sprite.spritecollideany(self, vertical_borders):
             self.vx = -self.vx
-        print('123')
+        print(self.vy, self.vy)
 
 
 def load_image(name, color_key=None):
@@ -127,14 +133,20 @@ def load_level(filename):
 # tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
 
 board_image = load_image('board1.png')
-
-ball_image = load_image('ball1.png')
-ball_image = pygame.transform.scale(ball_image, SIZE_BALL)
-
 board_width = board_image.get_width()
 board_height = board_image.get_height()
 board = Board(START_POSITION[0], START_POSITION[1])
+
+ball_image = load_image('ball1.png')
+ball_image = pygame.transform.scale(ball_image, SIZE_BALL)
 ball = Ball(START_POSITION[0], START_POSITION[1])
+
+Border(5, 5, WIDTH - 5, 5)
+Border(5, HEIGHT - 5, WIDTH - 5, HEIGHT - 5)
+Border(5, 5, 5, HEIGHT - 5)
+Border(WIDTH - 5, 5, WIDTH - 5, HEIGHT - 5)
+
+
 # Border(0, 0, WIDTH - 1, 0)  # верхняя
 # Border(0, 0,0, HEIGHT - 1)  # левая
 # Border(WIDTH - 1, 0, WIDTH - 1, HEIGHT - 1)  # правая
@@ -147,21 +159,22 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["ARKANOID" ]
+    # arkanoid", "",
+    #               "Правила игры",
+    #               "Если в правилах несколько строк,",
+    #               "приходится выводить их построчно"]
 
     fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
+    font = pygame.font.Font(None, 50)
+    text_coord = 300
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('SteelBlue'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 220
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
@@ -186,7 +199,7 @@ if __name__ == '__main__':
     # clock = pygame.time.Clock()
     # all_sprites = pygame.sprite.Group(Car('img.png', w))
     # level_map = load_level("map.map")
-    # screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
     all_sprites.update()
     running = True
     ball_true = False
@@ -197,7 +210,6 @@ if __name__ == '__main__':
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     ball_true = True
-                    print(ball_true)
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             if board.rect.x + OFFSET + board_width <= WIDTH:
                 board.rect.x += OFFSET
@@ -205,17 +217,17 @@ if __name__ == '__main__':
             if board.rect.x - OFFSET >= 0:
                 board.rect.x -= OFFSET
 
-        screen.fill(pygame.Color("black"))
+        screen.fill((255, 255, 255))
 
         # all_sprites.update()
         # all_sprites.draw(screen)
         # all_sprites.update()
+        border_group.draw(screen)
         board_group.draw(screen)
         ball_group.draw(screen)
 
         if ball_true:
             ball_group.update()
-            print('1')
         pygame.display.flip()
         clock.tick(FPS)
 
